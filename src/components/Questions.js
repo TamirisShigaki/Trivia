e85fa10197fc221a44ec2f546d97210eddce9396
Nikolas Firmo Cardoso <nickcardoso1@hotmail.com>
2022-04-06 18:41:37 -0300

    adiciona req.5

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -38,118 +38,43 @@ export default function Questions() {
     setQuestion(questions[position]);
     setPosition(position + 1);
   }
+
+  const magicNumber = 0.5;
+
   return (
     <div>
-      <h4
-        data-testid="question-category"
-      >
-        Category
-      </h4>
-      <p
-        data-testid="question-text"
-      >
-        Text
-      </p>
-      <div
-        data-testid="answer-options"
-      >
-        <button
-          data-testid="btn-play"
-          type="button"
-        /* onClick={ handleBtmclick } */
-        >
-          Answer 1
-        </button>
-        <button
-          data-testid="btn-play"
-          type="button"
-        /* onClick={ handleBtmclick } */
-        >
-          Answer 2
-        </button>
-        <button
-          data-testid="btn-play"
-          type="button"
-        /* onClick={ handleBtmclick } */
-        >
-          Answer 3
-        </button>
-        <button
-          data-testid="btn-play"
-          type="button"
-        /* onClick={ handleBtmclick } */
-        >
-          Answer 4
-        </button>
-        <button
-          data-testid="btn-play"
-          type="button"
-        /* onClick={ handleBtmclick } */
-        >
-          Answer 5
-        </button>
-      </div>
+      {question && (
+        <>
+          <h4
+            data-testid="question-category"
+          >
+            {question.category}
+          </h4>
+          <p
+            data-testid="question-text"
+          >
+            {question.question}
+          </p>
+        </>
+      )}
+
+      {question.type === 'multiple' && [...question.incorrect_answers,
+      question.correct_answer].sort(() => (Math.random() - magicNumber))
+        .map((element, index) =>
+          <button
+            data-testid="btn-play"
+            type="button"
+          /* onClick={ handleBtmclick } */
+          >
+            Answer 1
+          </button>
+        )
+        (
+          <div
+            data-testid="answer-options"
+          >
+        )}
     </div>
+    // https://javascript.info/task/shuffle  = MathRandom()
   );
 }
-
-/* class Questions extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      game: [],
-      counter: 0,
-    };
-  }
-
-  handleGame = async () => {
-    const { token, apiGetToken } = this.props;
-    const questionTotal = 5;
-    const limitResponse = 3;
-    const data = await getQuestion(questionTotal, token);
-
-    if (data.response_code === limitResponse) {
-      apiGetToken();
-    } else {
-      this.setState({
-        game: data && data.results,
-      });
-    }
-  }
-
-  handleCounter = () => {
-    this.setState((preview) => ({
-      counter: preview.counter + 1,
-    }));
-  }
-
-  render() {
-    const { game, counter } = this.state;
-    return (
-      <div>
-        <h1 data-testid="question-category">
-          { game.length > 0 && game[counter].category }
-        </h1>
-        <h2 data-testid="question-text">
-          { game.length > 0 && game[counter].question }
-        </h2>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => ({
-  token: state.token,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  apiGetToken: () => dispatch(getQuestion()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Questions);
-
-Questions.propTypes = {
-  token: PropTypes.string,
-}.isRequired;
- */
