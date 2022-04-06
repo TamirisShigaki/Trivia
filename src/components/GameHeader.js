e8e9800bca63a45de81582174b558626163fcd59
Leandro Bonfim <leandrobonfim15@homtial.com>
2022-04-06 11:57:49 -0300

    req 4: criado um header com as informações do player

---
+++ src/components/GameHeader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+export default function GameHeader() {
+  const { name, score, gravatarImage } = useSelector((state) => state.player);
+  console.log(gravatarImage, 'aqui');
+  return (
+    <div>
+      <img
+        data-testid="header-profile-picture"
+        src={ gravatarImage }
+        alt="user"
+      />
+      <span data-testid="header-player-name">{name}</span>
+      <span data-testid="header-score">{score}</span>
+    </div>);
+}
