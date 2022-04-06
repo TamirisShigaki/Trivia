6da92d66050d545f468e647b41a58feef5ecf573
Tamiris Shigaki <shigakiendo@gmail.com>
2022-04-06 09:32:13 -0300

    group 1: req 3 - criado botão e pagina de configuração do jogo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import logo from './trivia.png';
 import './App.css';
 import Login from './pages/Login';
+import Game from './pages/Game';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
@@ -14,6 +16,8 @@ export default function App() {
         </p>
         <Switch>
           <Route exact path="/" component={ Login } />
+          <Route path="/game" component={ Game } />
+          <Route path="/settings" component={ Settings } />
         </Switch>
       </header>
     </div>
