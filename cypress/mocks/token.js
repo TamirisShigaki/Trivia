b8b6c483c35048b836ac095e956359dc69a0eb09
Tamiris Shigaki <shigakiendo@gmail.com>
2022-04-15 20:35:40 -0300

    trivia

--- cypress/mocks/token.js
+++
@@ -1,7 +0,0 @@
-const tokenResponse = {
-  "response_code":0,
-  "response_message":"Token Generated Successfully!",
-  "token":"f00cb469ce38726ee00a7c6836761b0a4fb808181a125dcde6d50a9f3c9127b6"
-};
-
-module.exports = { tokenResponse };
